621928c412e1d1a2987256e448747e5d364d4b9c
shubhankarchdas <shubhankarchdas8@gmail.com>
2025-01-21 17:30:14 +0000

    conversion of datatypes

---
+++ Basic/3-conversionOperation.js
@@ -0,0 +1,28 @@
+let score = "50"
+
+console.log(typeof score)
+console.log(typeof (score))
+
+let valueIntNumber = Number(score)
+console.log(typeof valueIntNumber)
+console.log(valueIntNumber)
+
+//"50"- 50
+//"50abc"- NaN
+//true - 1
+//false - 0
+
+let isActive = ""
+let booleanIsActive = Boolean(isActive)
+console.log(booleanIsActive)
+
+//1 - true
+//0 - false
+//" " - true
+//"" - false
+//"shubhankar" - true
+
+let num = 50
+let stringnum = String(num)
+console.log(stringnum)
+console.log(typeof stringnum)
