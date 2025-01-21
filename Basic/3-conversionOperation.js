57b7afcf4f3c05d1e1c74cde332e6c2e719433a9
shubhankarchdas <shubhankarchdas8@gmail.com>
2025-01-21 18:04:38 +0000

    operations

--- Basic/3-conversionOperation.js
+++ Basic/3-conversionOperation.js
@@ -1,28 +1,73 @@
-let score = "50"
+// let score = "50"
 
-console.log(typeof score)
-console.log(typeof (score))
+// console.log(typeof score)
+// console.log(typeof (score))
 
-let valueIntNumber = Number(score)
-console.log(typeof valueIntNumber)
-console.log(valueIntNumber)
+// let valueIntNumber = Number(score)
+// console.log(typeof valueIntNumber)
+// console.log(valueIntNumber)
 
-//"50"- 50
-//"50abc"- NaN
-//true - 1
-//false - 0
+// //"50"- 50
+// //"50abc"- NaN
+// //true - 1
+// //false - 0
 
-let isActive = ""
-let booleanIsActive = Boolean(isActive)
-console.log(booleanIsActive)
+// let isActive = ""
+// let booleanIsActive = Boolean(isActive)
+// console.log(booleanIsActive)
 
-//1 - true
-//0 - false
-//" " - true
-//"" - false
-//"shubhankar" - true
+// //1 - true
+// //0 - false
+// //" " - true
+// //"" - false
+// //"shubhankar" - true
 
-let num = 50
-let stringnum = String(num)
-console.log(stringnum)
-console.log(typeof stringnum)
+// let num = 50
+// let stringnum = String(num)
+// console.log(stringnum)
+// console.log(typeof stringnum)
+
+//----------------Operations--------------------
+
+let value = 50
+let negvalue = -value
+//console.log(negvalue)
+// console.log(50+50)
+// console.log(50-50)
+// console.log(50*50)
+// console.log(50**50)
+// console.log(50/50)
+// console.log(50%50)
+
+let a = "hii "
+let b = "shubhankar"
+let c = a + b
+console.log(c)
+console.log(1+"2") //12
+console.log("1"+2) //12
+console.log("1"+2+2) //122
+console.log(1+2+"2") //32
+
+let n1,n2,n3
+n1 = n2 = n3 = 2 + 2
+
+let gamecounter = 100
+// gamecounter++;  //postfix
+++gamecounter;    //prefix
+console.log(gamecounter)
+
+// let x = 3;
+// const y = x++;
+// console.log(x,y)
+
+// let n = 3;
+// const m = ++n;
+// console.log(n,m)
+
+let x = 3;
+const y = x--;
+console.log(x,y)
+
+let n = 3;
+const m = --n;
+console.log(n,m)
