4b2bfcacc3e393cf37639ffc482065e18f7ddda9
shubhankarchdas <shubhankarchdas8@gmail.com>
2025-01-31 13:23:46 +0000

    datatype part2

---
+++ Basic/5-dataTypes-summary.js
@@ -0,0 +1,28 @@
+// primitive datatype
+
+// 7 types : String,Number,Boolean,null,undefiend,Symbol,BigInt
+
+const score = 100
+const id = Symbol('123')
+const anotherid = Symbol('123')
+console.log(id==anotherid);
+
+
+// const bigNumber = 1234456676787833333n
+
+
+// Reference datatype(non primitive): Array,Objects,Functions
+
+
+const heros = ["sss","aaa","bbb"]; //array
+let myObj = {
+    name: "shubhankar",  //{object}
+    age: 20,
+}
+
+const myFunction = function (){
+    console.log("Hey! shubhankar");
+    
+}
+
+console.log(typeof bigNumber);
