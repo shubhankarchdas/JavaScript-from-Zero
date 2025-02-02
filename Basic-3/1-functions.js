6ac7f62df92de5f906b038fa4ba87f6dd163a3b7
shubhankarchdas <shubhankarchdas8@gmail.com>
2025-02-02 06:19:05 +0000

    function with object and array

--- Basic-3/1-functions.js
+++ Basic-3/1-functions.js
@@ -38,8 +38,40 @@ function loginUserMessage(username){ //passing default value like this username 
 }
 
 console.log(loginUserMessage("shubhankar"));
-console.log(loginUserMessage(""));
-console.log(loginUserMessage());
+// console.log(loginUserMessage(""));
+// console.log(loginUserMessage());
+
+
+
+function calculateCartPrice(...num1){ //rest operator
+    return num1
+}
+// console.log(calculateCartPrice(200,400,500));
+
+const user = {
+    username: "shubhankar",
+    price: "2000"
+}
+function handleObject(anyobject){
+    console.log(`username is ${anyobject.username} and price is ${anyobject.price}`);
+}
+
+// handleObject(user)
+handleObject({
+    username:"shubh",
+    price: "200"
+})
+
+
+const myNewArray = [200,300,400]
+
+function returnsecondvalue(getarray){
+    return getarray[1]
+}
+
+// console.log(returnsecondvalue(myNewArray));
+console.log(returnsecondvalue([2000,3000,4000]));
+
 
 
 
