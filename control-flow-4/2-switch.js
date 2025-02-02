22baeb52bb93edad46fd87a5d046ade2b3f6703c
shubhankarchdas <shubhankarchdas8@gmail.com>
2025-02-02 14:26:20 +0000

    switch_case

---
+++ control-flow-4/2-switch.js
@@ -0,0 +1,19 @@
+const month = 4
+switch (month) {
+    case 1:
+        console.log("january");
+        break;
+    case 2:
+        console.log("feb");
+        break;
+    case 3:
+        console.log("march");
+        break;
+    case 4:
+        console.log("april");
+        break;
+
+    default:
+        console.log("does not match...");
+        break;
+}
