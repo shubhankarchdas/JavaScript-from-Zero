9ebec86ab8139ad9bb970cfe2dcf526d80f6b0da
shubhankarchdas <shubhankarchdas8@gmail.com>
2025-02-05 05:39:55 +0000

    while and do-while loop

---
+++ iterations-5/2-otherloops.js
@@ -0,0 +1,21 @@
+let i = 0
+while (i<10) {
+    console.log(`value of i is: ${i}`);
+    i++;
+}
+
+let myArray = ["username","email","pass"]
+
+let j = 0
+while (j<myArray.length) {
+    console.log(`output is: ${myArray[j]}`);
+    // console.log(`output is: ${j}`); // only printing the index
+    j++;
+}
+
+// let k = 0
+let k = 12
+do {
+    console.log(`k is ${k}`);
+    k++;
+} while (k<=10);
